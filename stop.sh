872c23e043c1d45a78ac6f4c822fb69045346c48
llinvokerl <1959600040@qq.com>
2018-10-24 21:47:00 +0800

    test

--- stop.sh
+++ stop.sh
@@ -4,4 +4,3 @@ DIR=$(cd "$(dirname "$1")"; pwd)
 cd $DIR/nginx_proxy && docker-compose stop
 cd $DIR/lemp && docker-compose stop
 cd $DIR/wordpress && docker-compose stop
-#docker network rm nginxproxy_damn
